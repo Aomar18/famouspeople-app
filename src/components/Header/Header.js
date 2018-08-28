89a72819513124bcabad6fdb03bd963944d48870
Ahmed <omar0121@umn.edu>
2018-08-28 14:52:52 -0500

    Changed font color of banner

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+// Header is the name of the component
+class Header extends Component {
+    render() {
+        return (
+            <header className="App-header">
+            <h1 className="App-title"> </h1>
+          </header>
+            
+         
+        );
+        };
+    }
+
+
+
+export default Header; 
